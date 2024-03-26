37371bf21f8bd388bf9d55b7b2da20236b531e14
vijay brat pandey <vijaybratpandey.com>
2024-03-26 19:55:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+var x = 0;
+function reveal ()
+ {
+    if(x==0) {
+        document.getElementById('b1').style.display = 'block';
+        x=1;
+    }
+    else {
+        document.getElementById('b1').style.display = 'none';
+        x=0;
+    }
+ }
+ function reveal1 ()
+ {
+    if(x==0) {
+        document.getElementById('b2').style.display = 'block';
+        x=1;
+    }
+    else {
+        document.getElementById('b2').style.display = 'none';
+        x=0;
+    }
+ }
+ function reveal2 ()
+ {
+    if(x==0) {
+        document.getElementById('b3').style.display = 'block';
+        x=1;
+    }
+    else {
+        document.getElementById('b3').style.display = 'none';
+        x=0;
+    }
+ }
+ function reveal3 ()
+ {
+    if(x==0) {
+        document.getElementById('b4').style.display = 'block';
+        x=1;
+    }
+    else {
+        document.getElementById('b4').style.display = 'none';
+        x=0;
+    }
+ }
+ function reveal4 ()
+ {
+    if(x==0) {
+        document.getElementById('b5').style.display = 'block';
+        x=1;
+    }
+    else {
+        document.getElementById('b5').style.display = 'none';
+        x=0;
+    }
+ }
